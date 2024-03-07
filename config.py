9fc904ca75e01b068db9d861553750eb145fa294
Rao Sahab <161572442+Blacklover16@users.noreply.github.com>
2024-03-07 22:10:04 +0530

    Update config.py
    Gh

--- config.py
+++ config.py
@@ -84,18 +84,18 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/8d9354990f647184fab9b.jpg"
+    "START_IMG_URL", https://te.legra.ph/file/e6c099ea40b122ad95274.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/50cb4c88a35a02bab053e.jpg"
+    "PING_IMG_URL",https://te.legra.ph/file/e6c099ea40b122ad95274.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/2e2f78610814092d61103.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/e6c099ea40b122ad95274.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/e6c099ea40b122ad95274.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/e6c099ea40b122ad95274.jpg
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/e6c099ea40b122ad95274.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/e6c099ea40b122ad95274.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ce5ffb3d5f383c781f234.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/e6c099ea40b122ad95274.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
