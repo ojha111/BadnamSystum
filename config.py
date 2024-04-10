b605e2ec53b0c0674e1c00022aedf632a41f4b64
ojha111 <152144787+ojha111@users.noreply.github.com>
2024-04-10 13:29:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "@AKKIOJHA",
+    "https://github.com/ojha111/BadnamSystum/edit/master",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
